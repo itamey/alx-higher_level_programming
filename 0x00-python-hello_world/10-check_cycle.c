1025ce3a70d16ae149fbad95cd2d2f7425543ab6
Shalom <eyoshalom@yahoo.com>
2022-11-29 00:07:28 -0800

    python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * check_cycle - search infinite loop on linkedlists
+ * @list: linkedlist head *
+ * Return: 1 if cycle found, 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *forwd = list;
+	listint_t *back = list;
+
+	if (!list || !list->next)
+		return (0);
+
+	back = back->next;
+	forwd = forwd->next->next;
+
+	for (; forwd && back && forwd->next;)
+	{
+		if (back == forwd)
+			return (1);
+
+		back = back->next;
+		forwd = forwd->next->next;
+
+	}
+	return (0);
+}
